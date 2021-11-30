483900514368f29c28d6c1cbaecbfac92a8d9c2e
leogaab <leonelgaab@gmail.com>
2021-11-30 10:35:35 -0300

    Added icons to variables, neede to recheck they work, added colors variable

---
+++ src/components/Icons/iconNames.js
@@ -0,0 +1,22 @@
+const IconNames = {
+    electric: 'electric',
+    bug2: 'bug',
+    grass: 'grass',
+    normal: 'normal',
+    ghost: 'ghost',
+    fighting: 'fighting',
+    flying: 'flying',
+    poison: 'poison',
+    ground: 'ground',
+    rock: 'rock',
+    steel: 'steel',
+    fire: 'fire',
+    water: 'water',
+    psychic: 'psychic',
+    ice: 'ice',
+    dragon: 'dragon',
+    dark: 'dark',
+    fairy: 'fairy',
+}
+
+export default IconNames
---
+++ src/components/theme.js
@@ -0,0 +1,29 @@
+const theme = {
+  colors: {
+    electric: '#FEB620',
+    bug: '#869808',
+    grass: '#6FC230',
+    normal: '#A7A28E',
+    ghost: '#5D5EA5',
+    fighting: '#7D361E',
+    flying: '#607ADB',
+    poison: '#90418C',
+    ground: '#CCB160',
+    rock: '#9C8831',
+    steel: '#9696A3',
+    fire: '#CC2100',
+    water: '#3F8EE1',
+    psychic: '#E25183',
+    ice: '#7BCFF8',
+    dragon: '#7763DA',
+    dark: '#352C25',
+    fairy: '#EEB3F3',
+    unknown: '#050A0C',
+    shadow: '#101010',
+    black: '#000',
+    white: '#FFF',
+    blue: '#003A70',
+  },
+}
+
+export default theme
