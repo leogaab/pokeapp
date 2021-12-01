7df005cfdfc1451c70c53d7b01161f91970edecd
leogaab <leonelgaab@gmail.com>
2021-11-30 23:25:46 -0300

    Added router, deleted deprecated app styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import Pokemon from "./pkmn/pokemon";
+import styled from 'styled-components'
+import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PokemonList from './components/PokemonList';
+import Pokemon from './components/Pokemon';
+
+const Container = styled.div`
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-      <Pokemon />
-  );
+      <Container>
+        <BrowserRouter>
+          <Navbar />
+          <Searchbar></Searchbar>
+          <Routes>
+            <Route exact path="/pokemon" element={<PokemonList />}></Route>
+            <Route exact path="/pokemon/:id" element={<Pokemon />}></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Container>
+  )
 }
 
 export default App;
